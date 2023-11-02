293b66c239cb1d6f5e2c60f954e444e90b2cb138
jinXerath <ilwlsghksli@gmail.com>
2023-11-02 23:12:39 +0900

    first commit!

---
+++ forestory/src/main/java/com/forestory/ForestoryApplication.java
@@ -0,0 +1,13 @@
+package com.forestory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForestoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForestoryApplication.class, args);
+	}
+
+}
---
+++ forestory/src/test/java/com/forestory/ForestoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forestory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForestoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
